75bd42364866f1dd0006759f7831cdd034d62656
ExistOrLive <2068531506@qq.com>
2024-03-06 02:22:40 +0800

    更新

--- DownloadSecretFile/DownloadSecrectFile.py
+++ DownloadSecretFile/DownloadSecrectFile.py
@@ -7,11 +7,11 @@ def main():
 
    token = sys.argv[1]
 
-   result = requests.get("https://api.github.com/repos/ExistOrLive/SecretFile/contents/GiteeClient/ZLGiteeAppKey.h",
+   result = requests.get("https://api.github.com/repos/ExistOrLive/SecretFile/contents/GiteeClient/ZLGIteeAppKey.swift",
                           headers={"Authorization":"token "+token,"Accept":"application/vnd.github.v3.raw+json"})
    if result.status_code == 200 :
-       open("ZLGiteeAppKey.h",'wb').write(result.content)
-       print("ZLGiteeAppKey.h download success")
+       open("ZLGiteeAppKey.swift",'wb').write(result.content)
+       print("ZLGiteeAppKey.swift download success")
    else :
        print(result)
 
